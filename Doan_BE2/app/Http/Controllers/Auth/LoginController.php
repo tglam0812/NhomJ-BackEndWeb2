278908f37f6bd0d5a4d434932c853d0b995cfc00
vyva2004 <vyva2004@gmail.com>
2025-05-08 14:19:23 +0700

    chuc nang dang nhap

---
+++ Doan_BE2/app/Http/Controllers/Auth/LoginController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Login Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller handles authenticating users for the application and
+    | redirecting them to your home screen. The controller uses a trait
+    | to conveniently provide its functionality to your applications.
+    |
+    */
+
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+       // $this->middleware('guest')->except('logout');
+       // $this->middleware('auth')->only('logout');
+    }
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+           
+            return redirect()->intended('/');
+        }
+
+        return back()->withInput();
+    }
+    //logout
+    public function logout()
+    {
+        if(Auth::logout())
+        {
+           // return redirect('/login');
+            return redirect('/');
+        };
+        return redirect('/');
+    }
+}
