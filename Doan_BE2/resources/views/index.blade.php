d048096b64e693d9a359293d6291b2ae45303a49
tglam0812 <trangialam2004@gmail.com>
2025-05-28 21:33:54 +0700

    fix 1-12, update footer

--- Doan_BE2/resources/views/index.blade.php
+++ Doan_BE2/resources/views/index.blade.php
@@ -105,19 +105,7 @@ Thương Mại Điện Tử
 				</a>
 				@endforeach
 			</div>
-			<div class="flex-w flex-c-m m-tb-10">
-				<div class="flex-c-m stext-106 cl6 size-104 bor4 pointer hov-btn3 trans-04 m-r-8 m-tb-4 js-show-filter">
-					<i class="icon-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-filter-list"></i>
-					<i class="icon-close-filter cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-					Filter
-				</div>
 
-				<div class="flex-c-m stext-106 cl6 size-105 bor4 pointer hov-btn3 trans-04 m-tb-4 js-show-search">
-					<i class="icon-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-search"></i>
-					<i class="icon-close-search cl2 m-r-6 fs-15 trans-04 zmdi zmdi-close dis-none"></i>
-					Search
-				</div>
-			</div>
 
 			<!-- Search product -->
 			<form method="GET" action="{{ route('home') }}">
@@ -379,19 +367,20 @@ Thương Mại Điện Tử
 		</div>
 	</div>
 </section>
-<!-- Banner -->	
+<!-- Banner -->
 <!-- Product -->
 @endsection
 @section('custom-scripts')
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 @if(session('success'))
 <script>
-    Swal.fire({
-        icon: 'success',
-        title: '{{ session('success') }}',
-        showConfirmButton: false,
-        timer: 3000
-    });
+	Swal.fire({
+		icon: 'success',
+		title: '{{ session('
+		success ') }}',
+		showConfirmButton: false,
+		timer: 3000
+	});
 </script>
 @endif
 @endsection
